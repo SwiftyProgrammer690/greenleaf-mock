3e8ec7f29f9bf17b2bc7fd91a7accdd964ddd033
Swarit Choudhari <68472469+SwiftyProgrammer690@users.noreply.github.com>
2024-07-24 20:55:14 +0000

    start working on the about me page while addig pther pages

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-alert("This is a mock website made by Swarit Choudhari for practice! If this is resembling any real company, it is PURELY UNINTENTIONAL...")
+//alert("This is a mock website made by Swarit Choudhari for practice! If this is resembling any real company, it is PURELY UNINTENTIONAL...")
